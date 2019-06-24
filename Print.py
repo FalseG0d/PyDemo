476cfc64ed0695b923e59cb04e2f0c87c98a1942
FalseG0d <u.garg.10@gmail.com>
2019-06-24 20:35:00 +0530

    Updated PyDemo

---
+++ Print.py
@@ -0,0 +1,7 @@
+a="First Name"
+b="Last Name"
+print("Hello my name is {} {}".format(a,b)) # format can be used put variables in {}
+print("%0.4f"%(3/2)) # correct till 4 decimal spaces
+print("hi","this","is")
+print("hi","this","is",sep='+')
+print("hi","this","is",end='+')
