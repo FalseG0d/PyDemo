c5df97d85ea39ae86319fad3dc8949cf35e4cafd
FalseG0d <u.garg.10@gmail.com>
2019-06-25 08:41:31 +0530

    Updated PyDemo

--- Inputs.py
+++ Inputs.py
@@ -1,2 +1,3 @@
 a=input("Inputs a String by default:")
 a=int(intput("Inputs a string converts into into using int function"))
+# we can use other functions like float() to convert an input to any data type of our choosing
